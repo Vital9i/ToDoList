2198282c7342858ca4599b02f1de3014bc96ba47
Vital9i <vitalikdoiniak@gmail.com>
2024-12-03 20:26:55 +0300

    sprint1 lesson2(complete2.0)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import { Todolist} from './components/Todolist';
+import {Todolist} from './components/Todolist';
 
 export  type TaskNameType = 'All' | 'Active' | 'Completed'
 
@@ -8,7 +8,7 @@ function App() {
 
     const titleText1 = 'What to learn-1'
 
-let [tasks, setTasks] = useState([
+    let [tasks, setTasks] = useState([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'ReactJS', isDone: false},
@@ -17,33 +17,42 @@ let [tasks, setTasks] = useState([
         {id: 6, title: 'RTK query', isDone: false},
     ])
 
-
-
     const removeTasks = (taskId: number) => {
         console.log(taskId)
         setTasks(tasks.filter(task => task.id !== taskId))
     }
 
-    // let [taskNameForDrushlag,setTaskNameForDrushlag] = useState<TaskNameType>('All')
-    //
-    // const changeTasks = (taskName: TaskNameType) => {
-    //     console.log(taskName)
-    //     setTaskNameForDrushlag(taskName);
-    // }
-    // let drushlaq = tasks
-    //     if (taskNameForDrushlag === 'Completed') {
-    //         drushlaq=tasks.filter(el => el.isDone)
-    //     }
-    //     if (taskNameForDrushlag === 'Active') {
-    //         drushlaq=tasks.filter(el => !el.isDone)
-    //     }
+    let [taskNameForDrushlag, setTaskNameForDrushlag] = useState('All')
+
+    const changeTasks = (taskName: TaskNameType) => {
+        console.log(taskName)
+        setTaskNameForDrushlag(taskName);
+    }
+
+
+    let drushlaq = tasks;
+    switch (taskNameForDrushlag) {
+        case 'All':
+            drushlaq = tasks;
+            break;
+        case 'Completed':
+            drushlaq = tasks.filter(el => el.isDone);
+            break;
+        case 'Active':
+            drushlaq = tasks.filter(el => !el.isDone);
+            break;
+        default:
+            drushlaq = tasks;
+            break;
+    }
+
 
     return (
         <div className='App'>
             <Todolist title={titleText1}
-                      tasks={tasks}
+                      tasks={drushlaq}
                       removeTasks={removeTasks}
-                      // changeTasks={changeTasks}
+                      changeTasks={changeTasks}
             />
         </div>
     );
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Button} from './Button';
 import {TaskNameType} from "../App";
 
 export type TodolistProps = {
     title: string
     tasks: TaskProps[]
-    removeTasks: (taskId:number) => void
-    changeTasks?: (taskName:TaskNameType) => void
+    removeTasks: (taskId: number) => void
+    changeTasks: (taskName: TaskNameType) => void
 }
 
 type TaskProps = {
@@ -15,73 +15,45 @@ type TaskProps = {
     isDone: boolean
 }
 
+export const Todolist = ({title, tasks, removeTasks, changeTasks}: TodolistProps) => {
 
-
-export const Todolist = ({title, tasks,removeTasks}: TodolistProps) => {
-    let [taskNameForDrushlag,setTaskNameForDrushlag] = useState('All')
-
-    const changeTasks = (taskName: TaskNameType) => {
-        console.log(taskName)
-        setTaskNameForDrushlag(taskName);
-    }
-
-    const drushlaqFoo=()=> {
-        let drushlaq = tasks;
-        switch (taskNameForDrushlag) {
-            case 'All':
-                drushlaq = tasks;
-                break;
-            case 'Completed':
-                drushlaq = tasks.filter(el => el.isDone);
-                break;
-            case 'Active':
-                drushlaq = tasks.filter(el => !el.isDone);
-                break;
-            default:
-                drushlaq = tasks;
-                break;
-        }
-        return drushlaq
-    }
-
-    let filteredTasks = drushlaqFoo()
-        return (
+    return (
+        <div>
+            <h3>{title}</h3>
             <div>
-                <h3>{title}</h3>
-                <div>
-                    <input/>
-                    <Button title='+'/>
-                </div>
-                {
-                    tasks.length === 0
-                        ? <p>тасок нет</p>
-                        : <ul>
-                            {filteredTasks.map((task) => {
-                                return (
-                                    <li key={task.id}>
-                                        <button onClick={() => {
-                                            removeTasks(task.id)
-                                        }}>X
-                                        </button>
-                                        <span>{task.title}</span>
-                                        <input type="checkbox" checked={task.isDone}/>
-                                    </li>)
-                            })}
-                        </ul>}
-                <div>
-                    <button onClick={() => {
-                        changeTasks('All')
-                    }}>All
-                    </button>
-                    <button onClick={() => {
-                        changeTasks('Active')
-                    }}>Active
-                    </button>
-                    <button onClick={() => {
-                        changeTasks('Completed')
-                    }}>Completed
-                    </button>
-                </div>
+                <input/>
+                <Button title='+'/>
             </div>
-        );
-    }
+            {
+                tasks.length === 0
+                    ? <p>тасок нет</p>
+                    : <ul>
+                        {tasks.map((task) => {
+                            return (
+                                <li key={task.id}>
+                                    <button onClick={() => {
+                                        removeTasks(task.id)
+                                    }}>X
+                                    </button>
+                                    <span>{task.title}</span>
+                                    <input type="checkbox" checked={task.isDone}/>
+                                </li>)
+                        })}
+                    </ul>}
+            <div>
+                <button onClick={() => {
+                    changeTasks('All')
+                }}>All
+                </button>
+                <button onClick={() => {
+                    changeTasks('Active')
+                }}>Active
+                </button>
+                <button onClick={() => {
+                    changeTasks('Completed')
+                }}>Completed
+                </button>
+            </div>
+        </div>
+    );
+}
