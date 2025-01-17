97753c4f3cb2bd44b28b10d50ee188ba6391f674
Vital9i <vitalikdoiniak@gmail.com>
2025-01-18 02:12:41 +0300

    Week 3, dop lesson3 start

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,62 @@
-import { useState } from 'react'
-import './App.css'
-import { TodolistItem } from './TodolistItem'
-import { v4 as uuidv4 } from 'uuid';
+import React, {useState} from 'react';
+import './App.css';
+import {Country} from "./Country";
+import {v1} from "uuid";
 
-export type FilterValuesType = 'All' | 'Active' | 'Completed'
+export type BanknotsType = '' // создадим типы для banknotes -он может быть 'DOLLARS', 'RUBLS' или 'All'
+export type MoneyType = {
+    banknote: BanknotsType
+    nominal: any// не ленимся, убираем заглушку, и пишем правильный тип)
+    id: any// ложку за Димыча, за...
+}
 
-export const App = () => {
+let defaultMoney: any = [  // типизируем
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'RUB', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'RUB', nominal: 100, id: v1()},
+    {banknote: 'USD', nominal: 100, id: v1()},
+    {banknote: 'RUB', nominal: 100, id: v1()},
+]
 
-    const [filter, setFilter] = useState<FilterValuesType>('All')
 
-    const [tasks, setTasks] = useState([
-        { id: uuidv4(), title: 'HTML&CSS', isDone: true },
-        { id: uuidv4(), title: 'JS', isDone: true },
-        { id: uuidv4(), title: 'ReactJS', isDone: false },
-        { id: uuidv4(), title: 'Redux', isDone: true },
-        { id: uuidv4(), title: 'Angular', isDone: true },
-        { id: uuidv4(), title: 'Vue', isDone: false },
-    ])
+export const moneyFilter = (money: any, filter: any): any => {
+    //если пришел filter со значением 'All', то возвращаем все банкноты
+    //return money.filter... ну да, придется фильтровать
+}
 
-    let filteredTasks
 
-    switch (filter) {
-        case 'All':
-            filteredTasks = tasks
-            break;
-        case 'Active':
-            filteredTasks = tasks.filter((t) => !t.isDone)
-            break;
-        case 'Completed':
-            filteredTasks = tasks.filter((t) => t.isDone)
-            break;
+function App() {
+    // убираем заглушки в типизации и вставляем в качестве инициализационного значения defaultMoney
+    const [money, setMoney] = useState<any>([])
+    const [filterValue, setFilterValue] = useState<any>('')   // по умолчанию указываем все банкноты
+
+    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
+    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
+    const filteredMoney = moneyFilter(грошы, фильтръ)
+
+    const addMoney = (banknote: BanknotsType) => {
+        // Добавление денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
     }
 
-    const removeTask = (taskId: string) => {
-        setTasks(tasks.filter((t) => t.id !== taskId))
-    }
-
-    const filterTasks = (filterValues: FilterValuesType) => {
-        setFilter(filterValues)
-    }
-
-    const createTask = (taskTitle:string)=>{
-        setTasks([{ id: uuidv4(), title: taskTitle, isDone: false },...tasks])
+    const removeMoney = (banknote: BanknotsType) => {
+        // Снятие денег сделаем в последнюю очередь, после настройки фильтров и отрисовки денег
+       // const index = money.findIndex
+       //  if (index !== -1) {
+       //      setMoney(money.filter((el, i) => ...));
+       //  }
     }
 
     return (
-        <div className="app">
-            <TodolistItem title={'What to learn'}
-                tasks={filteredTasks}
-                removeTask={removeTask}
-                filterTasks={filterTasks}
-                createTask={createTask} />
+        <div className="App">
+            <Country
+                data={filteredMoney}   //отрисовать будем деньги после фильтрации
+                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
+            />
         </div>
-    )
+    );
 }
 
+export default App;
